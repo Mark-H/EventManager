30b35d302279dfada0b3ec2e20dd6fbee9560705
smooth-graphics <info@novolo.de>
2011-03-19 09:49:43 +0100

    + added german translation * corrected spelling error in englich translation
    Signed-off-by: smooth-graphics <info@novolo.de>

---
+++ core/components/eventmanager/lexicon/de/default.inc.php
@@ -0,0 +1,60 @@
+<?php
+/*
+ * EventManager
+ *
+ * Copyright 2010-2011 by Mark Hamstra (contact via www.markhamstra.nl)
+ *
+ * This file is part of EventManager, a tool to manage events and reservations for MODX.
+ *
+ * EventManager is free software; you can redistribute it and/or modify it under the
+ * terms of the GNU General Public License as published by the Free Software
+ * Foundation; either version 2 of the License, or (at your option) any later
+ * version.
+ *
+ * EventManager is distributed in the hope that it will be useful, but WITHOUT ANY
+ * WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR
+ * A PARTICULAR PURPOSE. See the GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License along with
+ * EventManager; if not, write to the Free Software Foundation, Inc., 59 Temple Place,
+ * Suite 330, Boston, MA 02111-1307 USA
+ *
+ * @package EventManager
+ * @sub-package lexicon-de
+ * @date: 2011-03-19
+ * @author: Anselm Hannemann ( http://www.anselm-hannemann.de/ ), Novolo Designagentur ( http://www.novolo.de/ )
+ */
+ 
+$_lang['eventmanager'] = 'Event Manager';
+$_lang['eventmanager.description'] = 'Verwalten Sie Ihre Veranstaltungen und Reservierungen.';
+
+/* Current tab */
+$_lang['eventmanager.current'] = 'Aktuelle Veranstaltungen';
+$_lang['eventmanager.current.description'] = 'Hier finden Sie eine Übersicht der aktuellen und bevorstehenden Veranstaltungen. Vergangene Veranstaltungen sind für bessere Übersicht in einem separaten Tab geführt.';
+$_lang['eventmanager.current.howtouse'] = 'Die untenstehende Liste zeigt alle relevanten Details. Um Veranstaltungsdetails zu ändern oder zu sehen, wie viele Reservierungen bereits gebucht wurden, klicken Sie per Rechtsklick auf die gewünschte Veranstaltung.';
+
+/* Past tab */
+$_lang['eventmanager.past'] = 'Vergangene Veranstaltungen';
+$_lang['eventmanager.past.description'] = 'Hier finden Sie eine Übersicht der vergangenen Veranstaltungen (länger als 24 Stunden vorbei).';
+$_lang['eventmanager.past.howtouse'] = 'Sie können die gebuchten Reservierungen der Veranstaltung per Rechtsklick einsehen.';
+
+/* Grid headers (Current + Past tab) */
+$_lang['eventmanager.event.eventid'] = '#';
+$_lang['eventmanager.event.title'] = 'Name';
+$_lang['eventmanager.event.description'] = 'Beschreibung';
+$_lang['eventmanager.event.date'] = 'Veranstaltungsdatum';
+$_lang['eventmanager.event.time'] = 'Veranstaltungszeit';
+$_lang['eventmanager.event.capacity'] = 'Kapazität';
+$_lang['eventmanager.event.last_signup'] = 'Registrierung bis';
+$_lang['eventmanager.event.last_signup.hours'] = 'Registrieren Sie sich bis Xh im Voraus';
+
+/* Context menu */
+$_lang['eventmanager.cm.viewreservations'] = 'Reservierungsbuchungen ansehen';
+$_lang['eventmanager.cm.modify'] = 'Details ändern';
+$_lang['eventmanager.cm.duplicate'] = 'Duplizieren';
+$_lang['eventmanager.cm.delete'] = 'Veranstaltung entfernen';
+
+/* Toolbar */
+$_lang['eventmanager.toolbar.newevent'] = 'Veranstaltung erstellen';
+
+?>
